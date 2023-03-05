58b581a47321ebfa5774c7fbdc72118e4111435a
lee seung ah <lee88067@gmail.com>
2023-03-05 19:33:09 +0900

    add reviews model

--- common/models.py
+++ common/models.py
@@ -5,6 +5,5 @@ class CommonModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-class Meta:
-    abstract = True
+    class Meta:
+        abstract = True
